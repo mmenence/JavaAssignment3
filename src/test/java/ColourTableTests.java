d60f4e06f6866c71e320c7be0991924563f09ac5
mm <digestive100@gmail.com>
2023-12-04 13:36:31 +0000

    Added Test to check Input restrictions

--- src/test/java/ColourTableTests.java
+++ src/test/java/ColourTableTests.java
@@ -1,16 +1,27 @@
 import com.example.java_assignment_3.ColourTable;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
+import java.util.Arrays;
 
 public class ColourTableTests {
     @Test
     public void ColourTable(){
 
+
         int colourTableInput = 2;
         int expectedOutput = 2;
-
         ColourTable colourTable = new ColourTable(colourTableInput);
-        Assertions.assertEquals(expectedOutput, colourTable.noOfPalleteColours);
+        Assertions.assertEquals(expectedOutput, colourTable.noOfPalleteColours, 0, "input and number of colours allowed in palette are not equal");
+        //Assertions.assertTrue(Math.pow(colourTable.noOfPalleteColours, 2) % 1  == 0 && colourTable.noOfPalleteColours < 1025, "input and number of colours allowed in palette are not equal");
+    }
+
+    @Test
+    public void ColourTableInputRestrictionTests(){
+        int colourTableInput = 3;
+        int[] powerArray = {2,4,8,16,32,64,128,256,512,1024};
+        ColourTable colourTable = new ColourTable(colourTableInput);
+
+        Assertions.assertTrue( Arrays.asList(powerArray).contains(colourTable.noOfPalleteColours) && colourTable.noOfPalleteColours < 1025, "Input is not a power of tow or is greater than 1204");
 
     }
 }
